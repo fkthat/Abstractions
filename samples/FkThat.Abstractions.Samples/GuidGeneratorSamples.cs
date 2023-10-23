b0b9cc8c9c66a84371788a4a6335c1a9b5c056e4
fkthat <fkthat@yandex.ru>
2023-10-23 12:49:00 +0300

    Remove Console

--- samples/FkThat.Abstractions.Samples/GuidGeneratorSamples.cs
+++ samples/FkThat.Abstractions.Samples/GuidGeneratorSamples.cs
@@ -2,22 +2,20 @@
 
 internal sealed class GuidGeneratorSamples : ISample
 {
-    private readonly IConsole _console;
     private readonly Func<string, IGuidGenerator> _factory;
 
-    public GuidGeneratorSamples(IConsole console, Func<string, IGuidGenerator> factory)
+    public GuidGeneratorSamples(Func<string, IGuidGenerator> factory)
     {
-        _console = console;
         _factory = factory;
     }
 
     public Task RunAsync(CancellationToken cancellationToken = default)
     {
         var guidGen = _factory("system");
-        _console.WriteLine();
-        _console.WriteLine($"System GUIDs:");
-        _console.WriteLine($"  {guidGen.NewGuid()}");
-        _console.WriteLine($"  {guidGen.NewGuid()}");
+        Console.WriteLine();
+        Console.WriteLine($"System GUIDs:");
+        Console.WriteLine($"  {guidGen.NewGuid()}");
+        Console.WriteLine($"  {guidGen.NewGuid()}");
         return Task.CompletedTask;
     }
 }
