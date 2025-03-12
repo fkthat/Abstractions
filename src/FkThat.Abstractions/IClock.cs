0f7ce5646285b4fba57f6d1ca44daf43e6b80028
fkthat <fkthat@yandex.ru>
2025-03-12 18:23:37 +0300

    Fix to XML comments.

--- src/FkThat.Abstractions/IClock.cs
+++ src/FkThat.Abstractions/IClock.cs
@@ -7,7 +7,7 @@ public interface IClock
 {
     /// <summary>
     /// Gets a <c cref="DateTimeOffset"/> object whose date and time are set to the current
-    /// Coordinated Universal Time (UTC) date and time and whose offset is <c c="TimeSpan.Zero"/>.
+    /// Coordinated Universal Time (UTC) date and time and whose offset is <see cref="TimeSpan.Zero"/>.
     /// </summary>
     DateTimeOffset UtcNow { get; }
 }
